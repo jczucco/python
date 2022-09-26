8d23e721942dc02a9d946a376180828948e5ca36
Jeronimo Zucco <jczucco@gmail.com>
2022-09-26 08:48:24 -0300

    Add destination address in log output

--- getlogip.py
+++ getlogip.py
@@ -63,9 +63,10 @@ response = client.search(
 )
 for hit in response['hits']['hits']:
     try:
-        print('"%s" | "%s" | "%s" | "%s" | "%s"' % (
+        print('"%s" | "%s" | "%s" | "%s" | "%s" | "%s"' % (
             hit['_source']['@timestamp'],
             hit['_source']['source']['address'],
+            hit['_source']['host']['ip'][0],
             hit['_source']['http']['request']['method'],
             hit['_source']['url']['original'],
             hit['_source']['user_agent']['original']
